edcb664b85806c4417ef4eab6c9b623db53cb8bf
Luiscarlosvd <lc26cr7@gmail.com>
2023-07-19 11:40:25 -0400

    Fix linters errors

--- methods_to_decode_morse_code.rb
+++ methods_to_decode_morse_code.rb
@@ -1,32 +1,32 @@
 def decode_char(char)
-    morse_hash = {
-      '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
-      '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
-      '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
-      '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
-      '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
-      '--..' => 'Z'
-    }
-    morse_hash[char]
+  morse_hash = {
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
+    '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
+    '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
+    '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+    '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
+    '--..' => 'Z'
+  }
+  morse_hash[char]
 end
 
 def decode_word(string)
-  arr = string.split(' ')
+  arr = string.split
   decoded_word = ''
-  arr.each { |n| 
-  decoded_word += decode_char(n)
-  }
+  arr.each do |n|
+    decoded_word += decode_char(n)
+  end
   decoded_word
 end
-  
+
 def decode_message(string)
   arr = string.split('   ')
   decode_message = ''
-  arr.each { |n|
-  decode_message += decode_word(n)
-  decode_message += " "
-  }
+  arr.each do |n|
+    decode_message += decode_word(n)
+    decode_message += ' '
+  end
   decode_message
 end
 
-puts decode_message(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
+puts decode_message('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
